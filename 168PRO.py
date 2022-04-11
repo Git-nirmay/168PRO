3f019d121f788f710df9ab0306fdecc97b4718df
Git-nirmay <79860151+Git-nirmay@users.noreply.github.com>
2022-04-11 23:23:50 +0530

    Add files via upload

---
+++ 168PRO.py
@@ -0,0 +1,24 @@
+class Citizen: 
+    def __init__(self,name,age,dob,id_number): 
+        self.bookname = book_name 
+        self.bookauthor = bookauthor
+        self.bookprice = bookprice 
+        self.bookpublished = bookpublished 
+    def add_citizen(self): 
+        print("Book Name: "+self.bookname) 
+        print("Book Author: "+str(self.bookauthor)) 
+        print("Book Price: "+self.bookprice) 
+        print("Book was Published: "+self.bookpublished) 
+        print("Book Added") 
+        citizen1 = Book("Harry Potter And The Philosopher`s Stone") 
+        citizen1 = Book Author("J.K Rowling") 
+        citizen1 = Book Price("1928") 
+        citizen1 = Book was Published("1997") 
+        citizen1.add_citizen() 
+        print("This Book Was Published 23 Years Ago")
+        citizen2 = Book("Diary Of A Wimpy Kid") 
+        citizen2 = Book Author("Jeff Kenny") 
+        citizen2 = Book Price("700") 
+        citizen2 = Book was Published("2017") 
+        citizen2.add_citizen() 
+        print("This Book Was Published 3 Years Ago")
